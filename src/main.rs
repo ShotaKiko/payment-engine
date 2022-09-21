33b44973f94157ca0a18e92a93d0a09e34d25cba
Shota Kiko <Shota.Kikozashvili@gmail.com>
2022-09-20 21:29:05 -0600

    added serde deserialize into TransactionRecord struct upon csv read

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,21 @@ use std::env;
 use std::error::Error;
 use std::process;
 use std::ffi::OsString;
+use csv::ReaderBuilder;
+use serde::Deserialize;
 
+
+#[derive(Debug, Deserialize)]
+struct TransactionRecord {
+    #[serde(rename = "type")]
+    transaction_type: String, //TODO: incorporate an enum check on strings to weed out invalid entry types -> csv error
+    #[serde(rename = "client")]
+    client_id: u16,
+    #[serde(rename = "tx")]
+    transaction_id: u32,
+    #[serde(deserialize_with = "csv::invalid_option")] // in case the below tx types have nulls, some absence of value designation etc.
+    amount: Option<f64>, //Option because dispute, resolve and chargeback types have no associated amount
+}
 
 
 fn main() {
@@ -13,9 +27,9 @@ fn main() {
  }
 
 
-//Pull file path from user input returning an error if a file path argument is not supplied
-//TODO: find a mmore specific error type
-
+ 
+ //TODO: find a mmore specific error type
+ //Pull file path from user input to read transactions to process
 fn get_file_path() -> Result<OsString, Box<dyn Error>> {
     match env::args_os().nth(1) {
         None => Err(From::from("expected 1 arguments(the csv file to read from), but got none")),
@@ -23,14 +37,22 @@ fn get_file_path() -> Result<OsString, Box<dyn Error>> {
     }
 }
 
-
+//Engine
 fn run_payment_engine() -> Result<(), Box<dyn Error>> {
-    let file_path = get_file_path()?; //Add a check for file path type (ie: confirm it is a .csv)
+    let file_path = get_file_path()?; //TODO: Add a check for file path type (ie: confirm it is a .csv)
 
-    let mut reader = csv::Reader::from_path(file_path)?;
+    parse_csv(file_path)?;
 
-    for record_result in reader.records() {
-        let record = record_result?;
+    Ok(())
+}
+
+//Parse csv into hashmap to run account statement logic
+fn parse_csv(file_path: OsString) -> Result<(), Box<dyn Error>> {
+
+    let mut reader = ReaderBuilder::new().trim(csv::Trim::All).from_path(file_path)?; //also trims all whitespace
+
+    for record_result in reader.deserialize() {
+        let record: TransactionRecord = record_result?;
 
         println!("{:?}", record);
     }
